3e622f02127766fbf0290e3e268a96c91ef1f06d
kili-byte <ys3160112@gmail.com>
2026-03-05 23:53:06 +0530

    new addition

---
+++ datatype.js
@@ -0,0 +1,68 @@
+// data type in javascript
+//primitive data type
+//let name="yashika";
+//console.log(typeof name);
+//let age=19;
+//console.log(typeof age)
+//let isStudent=true;
+//console.log(typeof isStudent);
+//let score;
+//console.log(typeof score);
+//let car=null;
+// console.log(typeof car);
+
+// let student={
+//     name:"yashika",age:19,isStudent : true, email:"abc@gmail.com"
+// // }
+
+// let a=10;
+// console.log(a+=3);
+// console.log(a-=3);
+// console.log(a*=3);
+// console.log(a/=3);
+// console.log(a);
+
+// let temp=20;
+// if(temp>30){
+//     console.log("It's a hot day");
+// }
+// else if(temp<15){
+//     console.log("It's a cold day");
+// }
+// else{
+//     console.log("It's a pleasant day");
+// }
+let day = 3;
+
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+
+    case 2:
+        console.log("Tuesday");
+        break;
+
+    case 3:
+        console.log("Wednesday");
+        break;
+
+    case 4:
+        console.log("Thursday");
+        break;
+
+    case 5:
+        console.log("Friday");
+        break;
+
+    case 6:
+        console.log("Saturday");
+        break;
+
+    case 7:
+        console.log("Sunday");
+        break;
+
+    default:
+        console.log("Invalid day number");
+}
