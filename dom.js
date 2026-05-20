39fe29f2a56929be015d893d21d44cbf24fbc3cd
kili-byte <ys3160112@gmail.com>
2026-05-20 22:41:19 +0530

    Added Files

--- dom.js
+++
@@ -1,5 +0,0 @@
-let newheading = document.getElementById("heading");
-newheading.textContent="DOM Manipulation in Javascript";
-newheading.style.color="blue";
-let paragraphs= document.getElementsByClassName("paragraphh");
-paragraphs.innerHTML=<strong>"This is a paragraph to deomnstrate DOM Manipulation.</strong>
